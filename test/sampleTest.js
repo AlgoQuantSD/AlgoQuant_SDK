c90aba4e56a327b0e233cea51f9d220ed2e3a102
Tim Yang <tyang098@gmail.com>
2023-01-18 13:58:06 -0500

    add sample test

---
+++ test/sampleTest.js
@@ -0,0 +1,22 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+chai.should();
+
+chai.use(chaiHttp);
+
+const API_URL = "https://55vb89e0gb.execute-api.us-east-1.amazonaws.com/Test";
+
+describe("GET /", () => {
+  it("it should return response code 200", (done) => {
+    chai
+      .request(API_URL)
+      .get("/test-auth")
+      .end((err, res) => {
+        res.should.have.status(200);
+        // res.body.should.have.property("success").eql(true);
+        // res.body.should.have.property("message").eql("API is live...");
+        done(err);
+      });
+  });
+});
